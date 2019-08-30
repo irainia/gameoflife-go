203bb8977a6ae397fbb4cdeb36858d82aa61bcbf
Irainia <anwarhidayat14@gmail.com>
2019-08-30 07:21:34 +0700

    [Irainia] refactor to extract extension check

--- io/file/filestream.go
+++ io/file/filestream.go
@@ -22,12 +22,18 @@ func New(path string) (*FileStream, error) {
 	if path == "" {
 		return nil, errors.New(PathEmptyError)
 	}
-
-	splitPath := strings.Split(path, ".")
-	if fmt.Sprintf(".%s", splitPath[len(splitPath)-1]) != FileExtension {
+	if !isExtensionValid(path) {
 		return nil, errors.New(InvalidExtensionError)
 	}
 
 	var fileStream = FileStream{}
 	return &fileStream, nil
+}
+
+func isExtensionValid(path string) bool {
+	splitPath := strings.Split(path, ".")
+	if fmt.Sprintf(".%s", splitPath[len(splitPath)-1]) == FileExtension {
+		return true
+	}
+	return false
 }
