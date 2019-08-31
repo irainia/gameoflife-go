c40c9e670b56e7bf63393b1d243cc6ec1df74996
Irainia <anwarhidayat14@gmail.com>
2019-08-31 15:16:00 +0700

    [Irainia] pass spec for new should return nil and error for no input stream value

--- param/param.go
+++ param/param.go
@@ -2,15 +2,17 @@ package param
 
 import (
 	"errors"
+	"strings"
 
 	"github.com/Irainia/gameoflife-go/io"
 )
 
 const (
-	NilArgsError           = "args is nil"
-	EmptyArgsError         = "args is empty"
-	NoInputStreamTypeError = "no input stream type provided"
-	NoSeparatorError       = "no separator"
+	NilArgsError            = "args is nil"
+	EmptyArgsError          = "args is empty"
+	NoInputStreamTypeError  = "no input stream type provided"
+	NoInputStreamValueError = "no input stream value provided"
+	NoSeparatorError        = "no separator"
 )
 
 type Param struct {
@@ -27,8 +29,13 @@ func New(args []string, custom ...interface{}) (*Param, error) {
 	if len(args) == 0 {
 		return nil, errors.New(EmptyArgsError)
 	}
-	if args[0] != "--streamtype" {
+
+	arg := strings.Split(args[0], "=")
+	if arg[0] != "--streamtype" {
 		return nil, errors.New(NoInputStreamTypeError)
 	}
-	return nil, errors.New(NoSeparatorError)
+	if args[0][len(args[0])-1] != '=' {
+		return nil, errors.New(NoSeparatorError)
+	}
+	return nil, errors.New(NoInputStreamValueError)
 }
