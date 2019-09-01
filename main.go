769c00b602549ca969a72e8ee0fdba6cebd306d8
Irainia <anwarhidayat14@gmail.com>
2019-09-01 23:26:19 +0700

    [Irainia] add entry point in main

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Irainia/gameoflife-go/cell"
+	"github.com/Irainia/gameoflife-go/param"
+)
+
+func main() {
+	args := os.Args
+
+	parameter, err := param.New(args[1:], nil, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reader := parameter.GetReader()
+	initialGeneration, err := reader.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cellState, err := cell.New(initialGeneration)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for i := 0; i <= parameter.GetNumOfGeneration(); i++ {
+		fmt.Println()
+		fmt.Printf("genereation %d\n", i)
+		fmt.Println(cellState)
+		cellState = cellState.GetNextState()
+	}
+
+	writer := parameter.GetWriter()
+	err = writer.Write(cellState.GetGeneration())
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
