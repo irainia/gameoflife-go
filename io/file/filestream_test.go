ebfcd6e291b3b4c8ef65f502dc494f2d95baa287
Irainia <anwarhidayat14@gmail.com>
2019-09-20 12:01:14 +0530

    [Irainia] refactor to group spec for write

--- io/file/filestream_test.go
+++ io/file/filestream_test.go
@@ -236,58 +236,60 @@ func TestRead(t *testing.T) {
 	})
 }
 
-func TestWriteShouldReturnErrorForNilGeneration(t *testing.T) {
-	path := fmt.Sprintf("%s%s", cellDirectory, gliderCell)
-	fileStream, _ := file.New(path)
-	var nilGeneration [][]bool = nil
-	var expectedError error = errors.New(file.NilGenerationError)
+func TestWrite(t *testing.T) {
+	t.Run("should return error for nil generation", func(t *testing.T) {
+		path := fmt.Sprintf("%s%s", cellDirectory, gliderCell)
+		fileStream, _ := file.New(path)
+		var nilGeneration [][]bool = nil
+		var expectedError error = errors.New(file.NilGenerationError)
 
-	actualError := fileStream.Write(nilGeneration)
+		actualError := fileStream.Write(nilGeneration)
 
-	if actualError == nil {
-		t.Error("expected: not nil -- actual: nil")
-		return
-	}
-	if actualError.Error() != expectedError.Error() {
-		t.Errorf("expected: %s -- actual: %s", expectedError.Error(), actualError.Error())
-	}
-}
+		if actualError == nil {
+			t.Error("expected: not nil -- actual: nil")
+			return
+		}
+		if actualError.Error() != expectedError.Error() {
+			t.Errorf("expected: %s -- actual: %s", expectedError.Error(), actualError.Error())
+		}
+	})
 
-func TestWriteShouldReturnErrorForEmptyGeneration(t *testing.T) {
-	path := fmt.Sprintf("%s%s", cellDirectory, gliderCell)
-	fileStream, _ := file.New(path)
-	var emptyGeneration [][]bool = make([][]bool, 0)
-	var expectedError error = errors.New(file.EmptyGenerationError)
+	t.Run("should return error for empty generation", func(t *testing.T) {
+		path := fmt.Sprintf("%s%s", cellDirectory, gliderCell)
+		fileStream, _ := file.New(path)
+		var emptyGeneration [][]bool = make([][]bool, 0)
+		var expectedError error = errors.New(file.EmptyGenerationError)
 
-	actualError := fileStream.Write(emptyGeneration)
+		actualError := fileStream.Write(emptyGeneration)
 
-	if actualError == nil {
-		t.Error("expected: not nil -- actual: nil")
-		return
-	}
-	if actualError.Error() != expectedError.Error() {
-		t.Errorf("expected: %s -- actual: %s", expectedError.Error(), actualError.Error())
-	}
-}
+		if actualError == nil {
+			t.Error("expected: not nil -- actual: nil")
+			return
+		}
+		if actualError.Error() != expectedError.Error() {
+			t.Errorf("expected: %s -- actual: %s", expectedError.Error(), actualError.Error())
+		}
+	})
 
-func TestWriteShouldReturnNilForValidGeneration(t *testing.T) {
-	path := fmt.Sprintf("%s%s", cellDirectory, gliderCell)
-	fileStream, _ := file.New(path)
-	var validGeneration [][]bool = gliderGeneration
-	var expectedError error = nil
-	var expectedGeneration string = gliderGenerationString
+	t.Run("should return nil for valid generation", func(t *testing.T) {
+		path := fmt.Sprintf("%s%s", cellDirectory, gliderCell)
+		fileStream, _ := file.New(path)
+		var validGeneration [][]bool = gliderGeneration
+		var expectedError error = nil
+		var expectedGeneration string = gliderGenerationString
 
-	actualError := fileStream.Write(validGeneration)
+		actualError := fileStream.Write(validGeneration)
 
-	if actualError != expectedError {
-		t.Errorf("expected: nil -- actual: %s", actualError.Error())
-		return
-	}
-	actualGeneration, err := ioutil.ReadFile(path)
-	if err != nil {
-		panic(err)
-	}
-	if string(actualGeneration) != expectedGeneration {
-		t.Errorf("expected: %s -- actual: %s", expectedGeneration, string(actualGeneration))
-	}
+		if actualError != expectedError {
+			t.Errorf("expected: nil -- actual: %s", actualError.Error())
+			return
+		}
+		actualGeneration, err := ioutil.ReadFile(path)
+		if err != nil {
+			panic(err)
+		}
+		if string(actualGeneration) != expectedGeneration {
+			t.Errorf("expected: %s -- actual: %s", expectedGeneration, string(actualGeneration))
+		}
+	})
 }
